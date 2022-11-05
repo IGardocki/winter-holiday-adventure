fff9876c756b5b201a9d8e87165d70bbf6488c13
IGardocki <ian.d.gardocki@gmail.com>
2022-11-04 22:39:19 -0500

    added bg to start

--- src/Holidays.jsx
+++ src/Holidays.jsx
@@ -3,30 +3,26 @@ import { Button, Card, Container} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { HolidayContext } from './HolidayContext';
+import winterImage2 from './holidayImages/winterImage2.png'
 
 export const Holidays = () => {
     const navigate = useNavigate();
-    const {setHolidayDetails} = useContext(HolidayContext);
+    const { setHolidayDetails } = useContext(HolidayContext);
 
     return (
-        <Container>
+        <Container fluid style={{ display: "flex", flexDirection: "column", minHeight: "100vh", textAlign: "center", justifyContent: "center", backgroundImage: `url(${winterImage2})` }}>
             {winterHolidays.map(holiday => {
-                return (
-                    <Card>
-                        <Card.Img
-                            // style={{height: '20vh', width: '20vh'}} 
-                            varient='top'
-                            src={holiday.image} />
-                        {/* <Card.Body> */}
-                        <Card.Title>{holiday.name}</Card.Title>
-                        <Card.Text>{holiday.description}</Card.Text>
-                        <Button onClick={()=> {
-                            setHolidayDetails(holiday);
-                            navigate(`/holidays/${holiday.name}`)
+                return (                 
+                        <Card>
+                            <Card.Img src={holiday.image} style={{height: '10vh'}}/>
+                            <Card.Title>{holiday.name}</Card.Title>
+                            <Card.Text>{holiday.description}</Card.Text>
+                            <Button onClick={() => {
+                                setHolidayDetails(holiday);
+                                navigate(`/holidays/${holiday.name}`)
                             }}>
                                 Learn more about {holiday.name}</Button>
-                        {/* </Card.Body> */}
-                    </Card>
+                        </Card>
                 )
             })}
 
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,14 +1,19 @@
-import { Button, Card } from "react-bootstrap";
+import { Container, Button, Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import winterImage2 from './holidayImages/winterImage2.png';
 
 export const Welcome = () => {
     const navigate = useNavigate();
 
 
-    return(
-       <Card>
-        <Card.Title>Welcome, friends! There are so many winter holidays celebrated around the world! Would you like to learn more about them?</Card.Title>
-        <Button onClick={() => navigate('/holidays')}>Click to get started!</Button>
-       </Card>
+    return (
+        <Container fluid style={{ display: "flex", flexDirection: "column", height: "100vh", textAlign: "center", justifyContent: "center", backgroundImage: `url(${winterImage2})` }}>
+            <Card>
+                <Card.Title>Welcome, friends! There are so many winter holidays celebrated around the world! Would you like to learn more about them?</Card.Title>
+                <Button onClick={() => navigate('/holidays')}>Click to get started!</Button>
+            </Card>
+
+        </Container>
+
     )
 }
